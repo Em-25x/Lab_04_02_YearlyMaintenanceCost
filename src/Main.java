269fa16339780e7e153be93fbdb0422b3b8d009f
Em-25x <emmathehunter2007@gmail.com>
2025-09-15 23:20:14 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //A program that uses home maintenance costs for each of the four seasons and returns the total yearly maintenance costs.
+        // Again, be clear in the output by echoing the values for the seasonal costs when you display the combined total.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        double homeCostSummer = 150;
+        double homeCostWinter = 250;
+        double homeCostSpring = 100;
+        double homeCostFall = 125;
+        double homeMaintenanceCost = 0;
+   }
 }
